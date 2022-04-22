dd93e4cd24932149d7ce1e391b7c5c80a42faf8a
Ibrahim Tigrek <ibrahim.tigrek@gmail.com>
2022-04-22 17:23:54 +0300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 # setup.sh
 yum install git -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install node
 npm config set legacy-peer-deps true
